1d156f9f122193d2c50fec218b4e976150c2093f
divya.thakkar <divya.thakkar1618@gmail.com>
2025-01-29 23:54:08 -0700

    fix: port binding added

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 
 app = Flask(__name__)
+
+
 model = pickle.load(open("flight.pkl", "rb"))
 
 
@@ -245,3 +247,5 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port)
